edb6a07ab83e08b356dcf658fadc8f188f7678d6
PANKAJDEVESH <pankajdevesh3@gmail.com>
2019-09-22 23:04:53 +0530

    Updated: added Vue.js on assets. & updated UI

--- vue-js-example/assets/script.js
+++ vue-js-example/assets/script.js
@@ -2,21 +2,16 @@ var app = new Vue({
   el: '#app',
   data: {
   	users: [
-  		"Android JS",
-  		"Vue",
-  		"React",
-  		"Angular",
-  		"Phonon"
+  		{name: "Android JS", selected: false},
+  		{name: "Vue", selected: true},
+  		{name: "React", selected: false},
+  		{name: "Angular", selected: false},
+  		{name: "Phonon", selected: false}
   	]
   },
   methods: {
-  	remove(el){
-  		for( var i = 0; i < this.users.length; i++) { 
-		   	if ( this.users[i] === el) {
-		    	this.users.splice(i, 1); 
-		     	i--;
-		   	}
-		}
+  	toggle(el){
+      el.selected ^= true;
   	}
   }
 })
